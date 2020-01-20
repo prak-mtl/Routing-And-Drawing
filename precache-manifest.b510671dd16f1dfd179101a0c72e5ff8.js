a7527b1b3825557b9c9aa3f3da639cbc8f14b576
Mittal Prakhar <Prakhar.Mittal@lisec.com>
2020-01-20 14:21:02 +0400

    Updates

---
+++ precache-manifest.b510671dd16f1dfd179101a0c72e5ff8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e264393b9639c5bf7a24601cdc1bc925",
+    "url": "/Routing-And-Drawing/index.html"
+  },
+  {
+    "revision": "399dfa2ee0e1f13d9f9b",
+    "url": "/Routing-And-Drawing/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ef69cc7019ebcfc59446",
+    "url": "/Routing-And-Drawing/static/css/main.b110d5e9.chunk.css"
+  },
+  {
+    "revision": "399dfa2ee0e1f13d9f9b",
+    "url": "/Routing-And-Drawing/static/js/2.e0ac13f9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Routing-And-Drawing/static/js/2.e0ac13f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef69cc7019ebcfc59446",
+    "url": "/Routing-And-Drawing/static/js/main.5e4f12e0.chunk.js"
+  },
+  {
+    "revision": "32c41b07abaa0d375e99",
+    "url": "/Routing-And-Drawing/static/js/runtime-main.1e0167f6.js"
+  },
+  {
+    "revision": "677ff40b883ab1625e694534c5840e20",
+    "url": "/Routing-And-Drawing/static/media/LiSEC_eye.677ff40b.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Routing-And-Drawing/precache-manifest.b510671dd16f1dfd179101a0c72e5ff8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Routing-And-Drawing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
